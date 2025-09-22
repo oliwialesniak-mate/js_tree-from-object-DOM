b5f1d915c65fbdc9aea684ad4d609fb6f4854e39
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-22 02:58:29 +0200

    Implement createTree to generate nested ul/li from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,16 +20,24 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+/**
+ * Recursively creates a nested list (ul/li) from a nested object
+ * @param {HTMLElement} element - DOM element to append the tree to
+ * @param {Object} data - nested object to transform into a tree
+ */
 function createTree(element, data) {
   if (!data || typeof data !== 'object') return;
 
+  const keys = Object.keys(data);
+  if (!keys.length) return; // Don't create <ul> for empty objects
+
   const ul = document.createElement('ul');
 
-  Object.keys(data).forEach(key => {
+  keys.forEach(key => {
     const li = document.createElement('li');
     li.textContent = key;
 
-    // If the value is an object with keys, recursively create nested list
+    // Recursively create nested list if the value is an object
     if (data[key] && typeof data[key] === 'object' && Object.keys(data[key]).length) {
       createTree(li, data[key]);
     }
@@ -39,24 +47,5 @@ function createTree(element, data) {
 
   element.appendChild(ul);
 }
-
-// Example usage
-const tree = document.querySelector('#tree');
-const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
-  },
-  Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
-    },
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
-    },
-  },
-};
 
 createTree(tree, food);
