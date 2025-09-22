3ed6fd962d9633a66571cae83f9ccbeed3f3a834
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-22 02:53:06 +0200

    Create fully nested <ul> structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,42 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data || typeof data !== 'object') return;
+
+  const ul = document.createElement('ul');
+
+  Object.keys(data).forEach(key => {
+    const li = document.createElement('li');
+    li.textContent = key;
+
+    // If the value is an object with keys, recursively create nested list
+    if (data[key] && typeof data[key] === 'object' && Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  });
+
+  element.appendChild(ul);
 }
+
+// Example usage
+const tree = document.querySelector('#tree');
+const food = {
+  Drink: {
+    Wine: {},
+    Schnaps: {},
+  },
+  Fruit: {
+    Red: {
+      Cherry: {},
+      Strawberry: {},
+    },
+    Yellow: {
+      Banana: {},
+      Pineapple: {},
+    },
+  },
+};
 
 createTree(tree, food);
